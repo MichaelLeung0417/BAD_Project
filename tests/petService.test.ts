868c65f4f721b4b328e44c43fc0fc2411cbaaea1
Olevia Yuen <aivelo@icloud.com>
2022-08-04 12:45:18 +0800

    petSerice.test.ts

---
+++ tests/petService.test.ts
@@ -0,0 +1,9 @@
+import { PetService } from "../services/petService";
+
+describe("petService", () => {
+  it("should retrieve pet data from db", () => {});
+
+  it("should add new pet to database", () => {});
+
+  it("should retrieve a list of pets", () => {});
+});
