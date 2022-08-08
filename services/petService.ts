22c1030a19562f6859a83667e814deb15cee76e8
SevenBoii <assy2166@gmail.com>
2022-08-08 17:57:24 +0800

    create db folder

--- services/petService.ts
+++ services/petService.ts
@@ -3,6 +3,10 @@ import { Knex } from "knex";
 export class PetService {
   constructor(private knex: Knex) {}
 
+  async getAllPet() {
+    await this.getAllPet();
+  }
+
   async addPet() {
     await this.knex();
   }
