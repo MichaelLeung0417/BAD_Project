fe9b5c5390b10b093f5daebb92fe1dd9a76b3b01
SevenBoii <assy2166@gmail.com>
2022-08-13 13:52:03 +0800

    userService update

--- services/userService.ts
+++ services/userService.ts
@@ -1,26 +1,22 @@
-import { Client } from "pg";
+import { Knex } from "knex";
 import { hashPassword } from "../utilities/hash";
 import { User } from "../model/models";
 
 export class UserService {
-  constructor(private client: Client) {}
+  constructor(private knex: Knex) {}
 
   async getAllUser(username: string): Promise<User | undefined> {
-    return (
-      await this.client.query<User>(
-        `select id, username, password from users where users.username = $1`,
-        [username]
-      )
-    ).rows[0];
+    return await this.knex.raw<User>(
+      `select id, username, password from users where users.username = ?1`,
+      [username]
+    );
   }
 
   async insertUser(username: string, password: string): Promise<User> {
     const hashedPassword = await hashPassword(password);
-    return (
-      await this.client.query(
-        `INSERT INTO users (username, password) VALUES ($1,$2) RETURNING *`,
-        [username, hashedPassword]
-      )
-    ).rows[0];
+    return await this.knex.raw(
+      `INSERT INTO users (username, password) VALUES (?1,?2) RETURNING *`,
+      [username, hashedPassword]
+    );
   }
 }
