a58a33ba9f911416631407644e9b63cc242eccf5
SevenBoii <assy2166@gmail.com>
2022-08-13 20:53:37 +0800

    userService test case

--- tests/userService.test.ts
+++ tests/userService.test.ts
@@ -6,6 +6,12 @@ const knex = Knex(knexfile["test"]);
 describe("userService", () => {
   let userService: UserService = new UserService(knex);
 
+  it("should get data from users", async () => {
+    const usersData = await userService.getAllUser("1");
+
+    console.log(usersData);
+  });
+
   it("should insert data into db", async () => {
     const result = await userService.insertUser("testing", "123");
 
