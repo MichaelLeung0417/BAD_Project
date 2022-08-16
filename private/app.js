021d0b361a63c034cdb0e14f6662c4820040e86f
Olevia Yuen <aivelo@icloud.com>
2022-08-16 15:07:31 +0800

    removed something in main.py

--- private/app.js
+++ private/app.js
@@ -1,13 +1,75 @@
-const socket = io.connect();
+// const socket = io.connect();
 
-document.querySelector(".eatButton").addEventListener("click", () => {
-  socket.emit("eat", true);
-});
+// document.querySelector(".eatButton").addEventListener("click", () => {
+//   socket.emit("eat", true);
+// });
 
-document.querySelector(".arButton").addEventListener("click", () => {
-  socket.emit("AR", true);
-});
+// document.querySelector(".arButton").addEventListener("click", () => {
+//   socket.emit("AR", true);
+// });
 
-document.querySelector(".talkButton").addEventListener("click", () => {
-  socket.emit("talk", true);
-});
+// document.querySelector(".talkButton").addEventListener("click", () => {
+//   socket.emit("talk", true);
+// });
+
+// ANIMATION
+
+const spriteContainer = document.querySelector(".sprite-container");
+const sprite = document.querySelector("#sprite");
+
+let defaultLeft = 150;
+let defaultTop = 300;
+let defaultSpritePosition = 0;
+
+function spriteChange() {
+  if (defaultSpritePosition == 0) {
+    defaultSpritePosition = -250;
+  } else {
+    defaultSpritePosition = 0;
+  }
+  sprite.style["left"] = `${defaultSpritePosition}px`;
+}
+
+function movement() {
+  const number = Math.round(Math.random());
+
+  if (number == 0) {
+    const hMove = Math.round(Math.random());
+
+    if (hMove == 0) {
+      if (defaultLeft < 0) {
+        defaultLeft += 30;
+      } else {
+        defaultLeft -= 30;
+      }
+    } else {
+      if (defaultLeft > 350) {
+        defaultLeft -= 30;
+      } else {
+        defaultLeft += 30;
+      }
+    }
+  } else {
+    const vMove = Math.round(Math.random());
+
+    if (vMove == 0) {
+      if (defaultTop < 0) {
+        defaultTop += 30;
+      } else {
+        defaultTop -= 30;
+      }
+    } else {
+      if (defaultTop > 500) {
+        defaultTop -= 30;
+      } else {
+        defaultTop += 30;
+      }
+    }
+  }
+
+  spriteContainer.style["left"] = defaultLeft + "px";
+  spriteContainer.style["top"] = defaultTop + "px";
+}
+
+setInterval(movement, 500);
+setInterval(spriteChange, 1000);
