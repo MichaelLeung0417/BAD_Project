e4a2b5c94a35f9542330ab326870e2c5f7439a81
Olevia Yuen <aivelo@icloud.com>
2022-08-16 20:21:25 +0800

    reversed

--- public/speech.js
+++ public/speech.js
@@ -62,7 +62,7 @@ const submitPhoto = document
     console.log(response["filename"]);
 
     const analysePhoto = await fetch(
-      `https://smart-chipy.callings.me/analysePhoto?a=${response["filename"]}`
+      `https://smart-chipy.callings.me/analyzePhoto?a=${response["filename"]}`
     );
 
     const whatFruit = await analysePhoto.json();
